4c2d1a9d8b6c36c7b59eda1aa5de604a6f57cc14
pedroargento <peter.argent@gmail.com>
2014-01-03 01:02:23 +1100

    update to studio

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Studio::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   get "contact" => "contact#index"
-  get "group_training" => "group_training#index"
+  get "group_fitness" => "group_training#index"
   get "personal_training" => "personal_training#index"
   get "about" => 'about#index'
+  get "blog" => 'blog#index'
 end
