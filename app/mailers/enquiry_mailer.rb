58d5f13df240bf593129eda425265406231505dd
Pete Argent <pete@thecoderfactory.com>
2014-03-13 23:42:26 +1100

    update email for enquiry form

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -1,5 +1,5 @@
 class EnquiryMailer < ActionMailer::Base
-  default from: "Enquiry Form <pete@thecoderfactory.com"
+  default from: "Enquiry Form <marenjp@studioevolutionfitness.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,6 +8,6 @@ class EnquiryMailer < ActionMailer::Base
   #
   def received(enquiry)
     @enquiry = enquiry
-    mail(to: "peter.argent@gmail.com", subject: "An enquiry has been received")
+    mail(to: "marenjp@studioevolutionfitness.com", subject: "An enquiry has been received")
   end
 end
